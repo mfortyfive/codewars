a1330c98ef6dc62c485353ba43a0d944107a4fb0
mfortyfive <eta.tauri@gmail.com>
2021-12-17 16:14:02 -0800

    4kyu and 5kyu exercises

--- javascript/5kyu/orderWeight.js
+++ javascript/5kyu/orderWeight.js
@@ -22,3 +22,5 @@ const test = "2000 10003 1234000 44444444 9999 11 11 22 123";
 
 console.log("input:", test);
 console.log("result:", orderWeight(test));
+
+// returns '11 11 2000 10003 22 123 1234000 44444444 9999'
